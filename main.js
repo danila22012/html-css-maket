51f3bf8d4177850e986d6a9b4424d78a84e26847
danila22012 <danila222@ua.fm>
2019-12-25 14:23:20 +0200

    simple slider

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 'strict'
+const images = ['img/products/t-shirt.png',
+        'img/products/t-shirt2.png',
+        'img/products/t-shirt3.png',
+        'img/products/bag.png',
+        'img/products/wallet.png',
+        'img/products/cargo.png',
+        'img/products/shoe.png',
+        'img/products/shoes.png'
+]
+
+const btnLeft = document.querySelector('#btn-left');
+const btnRight = document.querySelector('#btn-right');
+const imgSlider = document.querySelectorAll('.slider-img');
+let i = 0;
+
+
+btnRight.onclick = ()=>{
+    
+     imgSlider.forEach( image => {
+        if( i==8 )i=0;
+        image.src = images[i++];
+        console.dir(image)
+    })
+    return i;
+
+}
+btnLeft.onclick = ()=>{
+
+     imgSlider.forEach( image => {
+        if( i==0 )i=7;
+        image.src = images[i--];
+        console.log(i)
+    })
+
+}
+
+
